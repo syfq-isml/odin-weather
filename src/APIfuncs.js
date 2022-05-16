d8757e07d930dc1b84c48d181c3980840cc50a23
syfq-isml <macamyesjer@gmail.com>
2022-05-16 20:48:19 +0800

    add function to display location name in header

--- src/APIfuncs.js
+++ src/APIfuncs.js
@@ -1,12 +1,17 @@
 // import fetch from "node-fetch";
 
-async function getLongLatData(name) {
+async function getLatLonData(name) {
 	let response = await fetch(
 		`http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=1&appid=0ef8f50d14aee0ab81ad9bef14aa0d56`
 	);
 	let data = await response.json();
-	console.log(`latlonData: ${data}`);
-	return { lat: data[0].lat, lon: data[0].lon };
+	console.log(data);
+	return {
+		name: data[0].name,
+		country: data[0].country,
+		lat: data[0].lat,
+		lon: data[0].lon,
+	};
 }
 
 async function getWeatherData_OVERALL(obj) {
@@ -14,18 +19,7 @@ async function getWeatherData_OVERALL(obj) {
 		`https://api.openweathermap.org/data/2.5/onecall?lat=${obj.lat}&lon=${obj.lon}&appid=0ef8f50d14aee0ab81ad9bef14aa0d56&units=metric`
 	);
 	let data = await response.json();
-	console.log(`getWeaterData : ${data}`);
-	return data;
+	return { current: data.current, hourly: data.hourly, daily: data.daily };
 }
 
-async function getWeatherData_CURRENT(obj) {
-	let currentWeather = { ...obj.current };
-}
-
-async function showData() {
-	let latLon = await getLongLatData("Singapore");
-	let overallWeather = await getWeatherData_OVERALL(latLon);
-	console.log(overallWeather);
-}
-
-export { showData };
+export { getLatLonData, getWeatherData_OVERALL };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
 import "./styles/normalize.css";
 import "./styles/styles.css";
 
-import { showData } from "./APIfuncs";
+import { getLatLonData, getWeatherData_OVERALL } from "./APIfuncs";
 
-console.log("All is good");
+const searchBar = document.querySelector("#search-bar");
+const searchBtn = document.querySelector("#search-btn");
+
+searchBtn.addEventListener("click", init);
+
+const test = document.querySelector("#test");
+test.addEventListener("click", disappear);
+
+function disappear() {
+	const elems = document.querySelectorAll("[data-appear]");
+	console.log(elems);
+	elems.forEach((elem) => elem.classList.toggle("hidden"));
+}
+
+// get data from input
+async function getDataFromInput() {
+	let latLon = await getLatLonData(searchBar.value);
+	let name = `${latLon.name}, ${latLon.country}`;
+	let dataObj = await getWeatherData_OVERALL(latLon);
+	return { name, weatherData: dataObj };
+}
+
+const displayWrapper = document.querySelector("#display-wrapper");
+
+function populateDisplayHeader(obj) {
+	const h1 = document.querySelector("#search-value");
+	console.log(h1);
+	console.log(obj.name);
+	h1.innerText = obj.name;
+}
+
+async function init() {
+	let first = await getDataFromInput();
+	populateDisplayHeader(first);
+}
